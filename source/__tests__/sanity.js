e8e222e2a07faad3809aad3a7f6be9a3df2e180a
Margie at School <margie@school.com>
2017-10-23 17:17:19 -0700

    tests starting to be working

--- source/__tests__/sanity.js
+++ source/__tests__/sanity.js
@@ -1,7 +1,39 @@
-describe('basic sanity test', function() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import ReactHome from '../ReactHome';
+import HomeButtons from '../HomeButtons';
+import {configure, shallow}from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({Adapter: new Adapter()});
+import ElfDebugEnzyme from '../ElfDebugEnzyme';
+const elfDebugEnzyme = new ElfDebugEnzyme(true, 'sanity');
+
+describe('basic sanity test', () => {
   it('is true true', function() {
-      
+
      expect(true).toBe(true);
   });
+
+    it('tests if we can load ReactHome', () => {
+
+      const div = document.createElement('div');
+      ReactDOM.render(<ReactHome/>, div);
+    });
+
+    it('tests if we can load HomeButtons', () => {
+
+        const div = document.createElement('div');
+        ReactDOM.render(<HomeButtons/>, div);
+    });
+
+    it('renders default value of H1 tag', () =>{
+        const wrapper = shallow(<ReactHome/>);
+        cont nineSign = <h1>An H1 Eelement in a React Components</h1>;
+        elfDebugEzyme.getlast...
+        ......;
+
+    });
 
 });
