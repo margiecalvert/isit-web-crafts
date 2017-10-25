ea8d6f7be2453edecdf4b802c79df1e17d3da304
Margie at School <margie@school.com>
2017-10-25 15:25:55 -0700

    working four tests

--- source/__tests__/sanity.js
+++ source/__tests__/sanity.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import ReactHome from '../ReactHome';
 import HomeButtons from '../HomeButtons';
-import {configure, shallow}from 'enzyme';
+import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-configure({Adapter: new Adapter()});
 import ElfDebugEnzyme from '../ElfDebugEnzyme';
 const elfDebugEnzyme = new ElfDebugEnzyme(true, 'sanity');
+configure({adapter: new Adapter()});
+import jQuery from 'jquery';
+global.jQuery = jQuery;
+global.$ = jQuery;
+//import '../fake-pub-sub';
+//import raf from '../temp-poly-fills';
 
-describe('basic sanity test', () => {
-  it('is true true', function() {
+describe('WebCrafts Sanity Test', function() {
 
-     expect(true).toBe(true);
-  });
+    'use strict';
 
-    it('tests if we can load ReactHome', () => {
-
-      const div = document.createElement('div');
-      ReactDOM.render(<ReactHome/>, div);
+    it('expects true to be true', function() {
+        expect(true).toBe(true);
     });
 
-    it('tests if we can load HomeButtons', () => {
-
+    it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<HomeButtons/>, div);
+        ReactDOM.render(<ReactHome/>, div);
     });
 
-    it('renders default value of H1 tag', () =>{
+    it('renders default value of H1 tag', () => {
         const wrapper = shallow(<ReactHome/>);
-        cont nineSign = <h1>An H1 Eelement in a React Components</h1>;
-        elfDebugEzyme.getlast...
-        ......;
-
+        const h1tag = <h1>An H1 element in a React Component</h1>;
+        elfDebugEnzyme.getLast(wrapper, 'h1', true);
+        expect(wrapper.contains(h1tag)).toEqual(true);
     });
-
 });
