a307a1fd9aadcb22be5fe722628e05277a608184
Margie at School <margie@school.com>
2017-11-01 17:10:14 -0700

    very confused about why the changes I made didn't stick

--- source/MakeHtmlDropDowns.js
+++ source/MakeHtmlDropDowns.js
@@ -4,6 +4,10 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import 'whatwg-fetch';
 
+
+const siteDirs = [];
+const destDirs = [];
+
 const styles = {
     customWidth: {
         width: 500,
@@ -17,18 +21,46 @@ class MakeHtmlDropDowns extends React.Component {
 
     constructor() {
         super();
-
         this.state = {
-            makeImage: 'Make Image',
-            makeHtml: 'Make HTML',
+            walk: 'Generate HTML',
+            siteDir: 'unknown',
+            destDir: 'unknown',
+            configSummary: [],
             value: 1
         };
+        //this.state = {
+           // makeImage: 'Make Image',
+           // makeHtml: 'Make HTML',
+           // value: 1
+       // };
         this.handleChange = this.handleChange.bind(this);
+    }
+
+    generateHtml() {
+        console.log(this.state.value);
+        console.log(siteDirs[this.state.value]);
+        //walking.runWalkReact('qSingle', this.state.siteDir, this.state.destDir);
+        const query = '/makers/walk?siteDirsIndex=' + this.state.value;
+        var that = this;
+        fetch(query)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(configSummary) {
+                console.log(JSON.stringify(configSummary, null, 4));
+                // CALL that.setState to **state.configSummary** to configSummary.htmlFilesWritten
+            })
+            .catch(function(ex) {
+                console.log('parsing failed', ex);
+            });
     }
 
     handleChange(event, index, value) {
         this.setState({value});
     }
+
+     // will need a pre tag where you can display the summary when you generate Html
+    //generateHTML button code will appear somewhere in this file for now; will refactor later
     /**
      * @typedef {Object} configSummary
      * @property {Object} siteDirs
@@ -45,6 +77,11 @@ class MakeHtmlDropDowns extends React.Component {
             .then(function (configSummary) {
                 //console.log('parsed json', JSON.stringify(configSummary, null, 4));
                 items.length = 0;
+                configSummary.siteDirs.forEach(function (dir, index) {
+                    const showDir = configSummary.baseDir + dir;
+                    items.push(<MenuItem value={index} key={index} primaryText={showDir} />);
+                });
+                //this code below has to be reconfigured to push into siteDirs not item
                 configSummary.siteDirs.forEach(function (dir, index) {
                     const showDir = configSummary.baseDir + dir;
                     items.push(<MenuItem value={index} key={index} primaryText={showDir} />);
@@ -72,6 +109,15 @@ class MakeHtmlDropDowns extends React.Component {
                     {items}
                 </DropDownMenu>
 
+                //below code must be modified for the second menu item
+                <DropDownMenu
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    style={styles.customWidth}
+                    autoWidth={false}
+                >
+                    {items}
+                </DropDownMenu>
                 <p>This is a DropDown component.</p>
             </div>
         </MuiThemeProvider>
