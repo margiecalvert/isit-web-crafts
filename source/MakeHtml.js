2ee0c73a5416e34ed9b2f5471679d4dc3270c2d7
margie at school <noone@nowhere.net>
2017-12-09 20:12:20 -0800

    webcraftsmakehtmlpage

--- source/MakeHtml.js
+++ source/MakeHtml.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MakeHtmlDropDowns from './MakeHtmlDropDowns';
-import MakeHtmlHomeButton from './MakeHtmlHomeButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class MakeHtml extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            home: 'Go Home'
+        }
+    }
+
+    goHome() {
+        $.publish('home', {
+            message: "The user wants to go home."
+        });
+    }
+
     render() {
 
         return <MuiThemeProvider>
             <div>
-                <MakeHtmlHomeButton/>
-                <MakeHtmlDropDowns/>
+                <RaisedButton
+                    style={buttonStyle}
+                    primary={true}
+                    onClick={this.goHome}>
+                    {this.state.home}
+                </RaisedButton>
+                <p>This is the React MakeHtml component.</p>
             </div>
         </MuiThemeProvider>;
     };
 }
+
+const buttonStyle = {
+    margin: '10px 10px 10px 0'
+};
 
 export default MakeHtml;
